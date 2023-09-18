977bbb92305aa698ceecbea96280f43edad585fb
Nota Name <email@thisisnotanemail.com>
2023-09-18 12:28:48 -0700

    update js

--- script.js
+++ script.js
@@ -4,17 +4,14 @@ function loadStaticContent() {
         .then(data => {
             document.getElementById("whytesla-title").textContent = "Why Tesla?";
             document.getElementById("whytesla-content").innerHTML = markdownit().render(data);
-
             return fetch('static_content.json');
         })
         .then(response => response.json())
         .then(jsonData => {
             document.getElementById("headerTitle").textContent = jsonData.headerTitle;
-
             for (let tooltip in jsonData.tooltips) {
                 document.getElementById("tooltip-" + tooltip).setAttribute("data-tooltip", jsonData.tooltips[tooltip]);
             }
-
             const glossarySection = document.getElementById("glossary");
             glossarySection.innerHTML = "";
             for (let term in jsonData.glossary) {
@@ -32,11 +29,10 @@ function updateStatement() {
     const electricityCost = parseFloat(document.getElementById("electricityCost").value).toFixed(2);
     const gasPrice = parseFloat(document.getElementById("gasPrice").value).toFixed(2);
     const mpg = parseFloat(document.getElementById("mpg").value).toFixed(2);
-    const federalCredit = parseFloat(document.getElementById("federalCredit").value).toFixed(2);
-    const referralCredit = parseFloat(document.getElementById("referralCredit").value).toFixed(2);
+    const federalCredit = parseFloat(document.getElementById("federalCredit").value);
+    const referralCredit = parseFloat(document.getElementById("referralCredit").value);
     const destinationFee = parseFloat(document.getElementById("destinationFee").value);
-const totalCredits = federalCredit + referralCredit + destinationFee;
-
+    const totalCredits = federalCredit + referralCredit + destinationFee;
     document.getElementById("basePrice").textContent = "$" + carPrice;
     document.getElementById("yearsHighlight").textContent = years;
     document.getElementById("milesYearHighlight").textContent = milesPerYear + " miles";
@@ -44,21 +40,13 @@ const totalCredits = federalCredit + referralCredit + destinationFee;
     document.getElementById("gasPriceHighlight").textContent = "$" + gasPrice;
     document.getElementById("mpgHighlight").textContent = mpg + " MPG";
     document.getElementById("totalCreditsHighlight").textContent = "$" + Math.abs(totalCredits);
-document.getElementById("tesla-initial-cost").textContent = "$" + (carPrice + totalCredits).toFixed(2);
-document.getElementById("tesla-gasoline-spend").textContent = "$" + calculateFuelSavings().toFixed(2);
-document.getElementById("tesla-electricity-spend").textContent = "$" + calculateElectricityCost().toFixed(2);
-document.getElementById("tesla-savings").textContent = "$" + (calculateFuelSavings() - calculateElectricityCost()).toFixed(2);
-document.getElementById("tesla-total-cost").textContent = "$" + (carPrice + totalCredits + calculateElectricityCost() - calculateFuelSavings()).toFixed(2);
-
 }
 
 function calculateTotal() {
     const initialCost = calculateAffordability();
     const fuelSavings = calculateFuelSavings();
     const electricityCost = calculateElectricityCost();
-    const totalCredits = calculateCredits();
     const resultingCost = initialCost + electricityCost - fuelSavings;
-
     document.getElementById("totalValue").textContent = "$" + resultingCost.toFixed(2);
 }
 
@@ -90,12 +78,6 @@ function calculateElectricityCost() {
     return totalKWh * electricityCost;
 }
 
-function calculateCredits() {
-    const federalCredit = parseFloat(document.getElementById("federalCredit").value);
-    const referralCredit = parseFloat(document.getElementById("referralCredit").value);
-    return Math.min(federalCredit, 0) + Math.min(referralCredit, 0);
-}
-
 function toggleTheme() {
     const theme = document.documentElement.getAttribute("data-theme");
     if (theme === "dark") {
@@ -117,10 +99,8 @@ function exportToJSON() {
         gasPrice: document.getElementById("gasPrice").value,
         mpg: document.getElementById("mpg").value
     };
-
     const blob = new Blob([JSON.stringify(data, null, 2)], { type: 'application/json' });
     const url = window.URL.createObjectURL(blob);
-
     const downloadAnchorNode = document.createElement('a');
     downloadAnchorNode.setAttribute("href", url);
     downloadAnchorNode.setAttribute("download", "calculator_values.json");
@@ -137,10 +117,8 @@ function handleFile() {
     const fileInput = document.getElementById('jsonInput');
     const file = fileInput.files[0];
     const reader = new FileReader();
-
     reader.onload = function(event) {
         const data = JSON.parse(event.target.result);
-
         document.getElementById("carPrice").value = data.carPrice || "";
         document.getElementById("federalCredit").value = data.federalCredit || "";
         document.getElementById("referralCredit").value = data.referralCredit || "";
@@ -150,12 +128,9 @@ function handleFile() {
         document.getElementById("electricityCost").value = data.electricityCost || "";
         document.getElementById("gasPrice").value = data.gasPrice || "";
         document.getElementById("mpg").value = data.mpg || "";
-
-        // Update the calculator and total after importing values
         updateStatement();
         calculateTotal();
     };
-
     reader.readAsText(file);
 }
 
@@ -172,8 +147,6 @@ function loadTeslaModel3Values() {
             document.getElementById("electricityCost").value = data.electricityCost || "";
             document.getElementById("gasPrice").value = data.gasPrice || "";
             document.getElementById("mpg").value = data.mpg || "";
-
-            // Update the calculator and total after loading values
             updateStatement();
             calculateTotal();
         })
@@ -187,17 +160,12 @@ function loadVehicleComparison() {
         .then(response => response.json())
         .then(data => {
             let comparison = data.comparison;
-
             let comparisonContent = '<table role="grid">';
-
-            // Header
             comparisonContent += '<thead><tr>';
             comparisonContent += '<th scope="col">Model X</th>';
             comparisonContent += '<th scope="col">Yukon Denali</th>';
             comparisonContent += '<th scope="col">Explanation</th>';
             comparisonContent += '</tr></thead>';
-
-            // Body
             comparisonContent += '<tbody>';
             for (let item of comparison) {
                 comparisonContent += `<tr>`;
@@ -207,9 +175,7 @@ function loadVehicleComparison() {
                 comparisonContent += `</tr>`;
             }
             comparisonContent += '</tbody>';
-
             comparisonContent += '</table>';
-
             document.getElementById('vehicle-comparison-content').innerHTML = comparisonContent;
         });
 }
@@ -221,7 +187,6 @@ document.addEventListener("DOMContentLoaded", function() {
             const referralLinks = data.referrals;
             const randomIndex = Math.floor(Math.random() * referralLinks.length);
             const selectedReferralLink = referralLinks[randomIndex];
-
             document.getElementById('referral-link').href = selectedReferralLink;
         });
 });
@@ -241,6 +206,7 @@ function loadToyotaCorollaValues() {
             console.error("Error loading Toyota Corolla values:", error);
         });
 }
+
 function updateGasolineStatement() {
     const carPrice = parseFloat(document.getElementById("gasoline-carPrice").value).toFixed(2);
     const years = document.getElementById("gasoline-years").value;
@@ -248,18 +214,13 @@ function updateGasolineStatement() {
     const gasPrice = parseFloat(document.getElementById("gasoline-gasPrice").value).toFixed(2);
     const mpg = parseFloat(document.getElementById("gasoline-mpg").value).toFixed(2);
     const totalGasCost = calculateGasolineCost().toFixed(2);
-
-    // Set the values in the HTML
     document.getElementById("gasoline-basePriceHighlight").textContent = "$" + carPrice;
     document.getElementById("gasoline-yearsHighlight").textContent = years;
     document.getElementById("gasoline-milesYearHighlight").textContent = milesPerYear + " miles";
     document.getElementById("gasoline-gasPriceHighlight").textContent = "$" + gasPrice;
     document.getElementById("gasoline-mpgHighlight").textContent = mpg + " MPG";
     document.getElementById("gasoline-totalValue").textContent = "$" + (parseFloat(carPrice) + parseFloat(totalGasCost)).toFixed(2);
-    document.getElementById("gasoline-gasoline-spend").textContent = "$" + calculateGasolineCost().toFixed(2);
-document.getElementById("gasoline-total-cost").textContent = "$" + (parseFloat(document.getElementById("gasoline-carPrice").value) + calculateGasolineCost()).toFixed(2);
 }
-
 
 function calculateGasolineCost() {
     const years = parseFloat(document.getElementById("gasoline-years").value);
@@ -270,7 +231,6 @@ function calculateGasolineCost() {
     const totalGallons = totalMiles / mpg;
     return totalGallons * gasPrice;
 }
-
 
 window.onload = function () {
     const inputs = document.querySelectorAll("input, select");
@@ -285,7 +245,6 @@ window.onload = function () {
     calculateTotal();
     loadVehicleComparison();
     updateGasolineStatement();
-
     const gasolineInputs = document.querySelectorAll("#gasoline-calculator input");
     for (let input of gasolineInputs) {
         input.addEventListener("input", function () {
